77abe93fa80e328720b885f95793258d126bc82e
Fazel Saeedi <fazeltsaeedi@yahoo.com>
2022-02-15 19:39:17 +0330

    Feat : todo app completed

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos : Todo[] = [] ;
+
+  inputTodo : string = "";
+
+  constructor() { console.log(this.todos);
+  }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content : 'First todo' ,
+        completed : false
+      },
+      {
+        content : 'Second todo' ,
+        completed : false
+      }
+    ]
+  }
+
+  toggleDone (id : number) : void
+  {
+
+  //  console.log(id);
+
+    this.todos?.map( (v , i ) =>
+    {
+        if(i == id)
+            v.completed = !v.completed ;
+
+
+        return v;
+
+    })
+
+  }
+
+
+
+  deleteTodo (id : number){
+    this.todos = this.todos.filter((v , i) => i !== id);
+  }
+
+  addTodo ()
+  {
+
+    this.todos.push({
+      content: this.inputTodo ,
+      completed : false
+    })
+
+  }
+
+}
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,8 @@
+export class Todo{
+  constructor(content: string , completed : boolean) {
+    this.content = content ;
+    this.completed = completed ;
+  }
+   content : string ;
+   completed : boolean ;
+}
